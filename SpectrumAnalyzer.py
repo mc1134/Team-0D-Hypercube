8f27175c219f9fd2ff9430caa9820ce315981bbc
mc1134 <j.michael.chen@gmail.com>
2018-01-13 00:36:51 -0800

    Added graphs

--- SpectrumAnalyzer.py
+++ SpectrumAnalyzer.py
@@ -23,7 +23,7 @@ data = stream.read(CHUNK)
 #data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype='b')[::2] + 127
 
 from scipy.io.wavfile import read
-a = read("wav/sound3.wav")
+a = read("wav/shortLastCarnival.wav")
 data_int = np.array(a[1],dtype=float)
 
 fig, ax = plt.subplots()
